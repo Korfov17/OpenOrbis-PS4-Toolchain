51ce39ae2cf7fc09d24c9e0d4cb21b8afafe7f02
Korfov17 <paco.80589@gmail.com>
2025-10-20 15:42:08 +0000

    Update main.c

--- samples/net_http/net_http/main.c
+++ samples/net_http/net_http/main.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
 #include <orbis/Http.h>
 #include <orbis/Ssl.h>
+#include <orbis/CommonDialog.h>
+#include <orbis/MsgDialog.h>
 #include <orbis/Net.h>
 #include <orbis/Sysmodule.h>
 
@@ -14,6 +17,65 @@
 
 #define NET_POOLSIZE 	(4 * 1024)
 
+// Dialog types
+#define MDIALOG_OK       0
+#define MDIALOG_YESNO    1
+
+
+static inline void _orbisCommonDialogSetMagicNumber(uint32_t* magic, const OrbisCommonDialogBaseParam* param)
+{
+    *magic = (uint32_t)(ORBIS_COMMON_DIALOG_MAGIC_NUMBER + (uint64_t)param);
+}
+
+static inline void _orbisCommonDialogBaseParamInit(OrbisCommonDialogBaseParam *param)
+{
+    memset(param, 0x0, sizeof(OrbisCommonDialogBaseParam));
+    param->size = (uint32_t)sizeof(OrbisCommonDialogBaseParam);
+    _orbisCommonDialogSetMagicNumber(&(param->magic), param);
+}
+
+static inline void orbisMsgDialogParamInitialize(OrbisMsgDialogParam *param)
+{
+    memset(param, 0x0, sizeof(OrbisMsgDialogParam));
+    _orbisCommonDialogBaseParamInit(&param->baseParam);
+    param->size = sizeof(OrbisMsgDialogParam);
+}
+
+int show_dialog(int dialog_type, const char * format, ...)
+{
+    OrbisMsgDialogParam param;
+    OrbisMsgDialogUserMessageParam userMsgParam;
+    OrbisMsgDialogResult result;
+
+    char str[0x800];
+    memset(str, 0, sizeof(str));
+
+    va_list opt;
+    va_start(opt, format);
+    vsprintf(str, format, opt);
+    va_end(opt);
+
+    sceMsgDialogInitialize();
+    orbisMsgDialogParamInitialize(&param);
+    param.mode = ORBIS_MSG_DIALOG_MODE_USER_MSG;
+
+    memset(&userMsgParam, 0, sizeof(userMsgParam));
+    userMsgParam.msg = str;
+    userMsgParam.buttonType = (dialog_type ? ORBIS_MSG_DIALOG_BUTTON_TYPE_YESNO_FOCUS_NO : ORBIS_MSG_DIALOG_BUTTON_TYPE_OK);
+    param.userMsgParam = &userMsgParam;
+
+    if (sceMsgDialogOpen(&param) < 0)
+        return 0;
+
+    do { } while (sceMsgDialogUpdateStatus() != ORBIS_COMMON_DIALOG_STATUS_FINISHED);
+    sceMsgDialogClose();
+
+    memset(&result, 0, sizeof(result));
+    sceMsgDialogGetResult(&result);
+    sceMsgDialogTerminate();
+
+    return (result.buttonId == ORBIS_MSG_DIALOG_BUTTON_ID_YES);
+}
 
 static int libnetMemId = 0, libhttpCtxId = 0, libsslCtxId = 0;
 
@@ -201,12 +263,28 @@ void http_end(void)
 	sceNetPoolDestroy(libnetMemId);
 }
 
-
 int main(void)
 {
-	http_init();
-	http_download("https://www.google.com/robots.txt", "/data/net_http.txt", 1);
-	http_end();
+    // Load the Message Dialog module
+    if (sceSysmoduleLoadModule(ORBIS_SYSMODULE_MESSAGE_DIALOG) < 0 ||
+        sceCommonDialogInitialize() < 0)
+    {
+        printf("Failed to initialize CommonDialog\n");
+        for(;;);
+    }
 
-	for (;;) {}
+    // Show a message dialog
+    if (show_dialog(MDIALOG_YESNO, "Do you like %s?", "OpenOrbis"))
+    {
+        show_dialog(MDIALOG_OK, "Descargado %s :)", "OpenOrbis");
+		http_init();
+		http_download("https://www.google.com/robots.txt", "/data/net_http.txt", 1);
+		http_end();
+    }
+    else
+    {
+        show_dialog(MDIALOG_OK, "No descargado %s :(", "OpenOrbis");
+    }
+
+    for(;;);
 }
